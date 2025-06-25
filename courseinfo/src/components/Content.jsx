eacf743d82c46836874c19b191fdfa0cd0537846
Alexander <dante690dark@gmail.com>
2025-06-24 23:00:25 -0500

    add new three components

--- courseinfo/src/components/Content.jsx
+++ courseinfo/src/components/Content.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Header = () => {
+const Content = () => {
   return <div></div>;
 };
 
-export default Header;
+export default Content;
---
+++ courseinfo/src/components/Total.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Total = () => {
+  return <div></div>;
+};
+
+export default Total;
