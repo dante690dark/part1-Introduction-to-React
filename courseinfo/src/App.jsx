afbf21412e1248175eeb3681a056d03a09e82437
Alexander <dante690dark@gmail.com>
2025-06-24 22:48:55 -0500

    create courseinfo react app using vite

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,42 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  const totalExercises = course.parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  );
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <p>
+        {course.parts[0].name} {course.parts[0].exercises}
+      </p>
+      <p>
+        {course.parts[1].name} {course.parts[1].exercises}
+      </p>
+      <p>
+        {course.parts[2].name} {course.parts[2].exercises}
+      </p>
+      <p>Number of exercises {totalExercises}</p>
+    </div>
+  );
+};
+
+export default App;
