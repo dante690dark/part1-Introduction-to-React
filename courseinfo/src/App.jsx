cf445f173ca53b62f3967b3a5ee1c237d3d2aad6
Alexander <dante690dark@gmail.com>
2025-06-25 11:31:43 -0500

    refactor code in javascript topic

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -4,25 +4,29 @@ import Content from './components/Content';
 import Total from './components/Total';
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  const { name, parts } = course;
 
   return (
     <>
-      <Header course={course} />
+      <Header name={name} />
       <Content parts={parts} />
       <Total parts={parts} />
     </>
--- courseinfo/src/components/Header.jsx
+++ courseinfo/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
 };
 
 export default Header;
